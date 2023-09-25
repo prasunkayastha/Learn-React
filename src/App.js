c50e6307ffc420a6532b01d4507a91bf6a93ce96
prasun kayastha <pkayastha31@gmail.com>
2023-09-25 10:15:23 +0545

    props and map

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from "react"
 import Navbar from "./component/Navbar"
 import Hero from "./component/Hero"
 import Card from "./component/card"
+import data from "./data"
 
 export default function App() {
+  const cards=data.map(item=>{
+    return(
+      <Card 
+      key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <section className="card-list">
+        {cards}
+      </section>
     </div>
   )
 }
--- src/component/card.js
+++ src/component/card.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className="card">
-            <img src="../images/Katie-Zaferes.png" className="card-image"/>
+            <img src={`../images/${props.img}`} className="card-image"/>
             <div className="card-stat">
                 <img src="../images/star.png" className="star"/>
-                <span>5.0</span>
-                <span className="grey">(6).</span>
-                <span className="grey">USA</span>
+                <span>{props.rating}</span>
+                <span className="grey">({props.review}).</span>
+                <span className="grey">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> /person</p>
+            <p className="card-title">Life Lessons with Katie Zaferes</p>
+            <p className="card-price"><span className="bold">From ${props.price}</span> /person</p>
         </div>
     )
 }
