a060cde8ae6b70aa804de867e621001cbcd25c1d
prasun kayastha <pkayastha31@gmail.com>
2023-09-26 09:40:13 +0545

    static web app

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ export default function App() {
   const cards=data.map(item=>{
     return(
       <Card 
-      key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
+        key={item.id}
+        item={item}
       />
     )
   })
